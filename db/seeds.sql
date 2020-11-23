da033691809400644e14cbcdcbf43def07b12f61
Julia <daubertjulia@yahoo.com>
2020-11-23 20:02:14 +0800

    Schmea and Seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+-- Insert a set of records.
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("Hamburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Chickenburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Fishburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Veggie Burger");
