bebec3a01f2c373f36bd6ccfe8c1f8a24582d793
Julia <daubertjulia@yahoo.com>
2020-11-23 16:31:13 +0800

    express

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 // *** Dependencies
 // =============================================================
 var express = require("express");
+
+
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('This is my Burger App')
+})
+ 
+//Heroku requires 8080
+app.listen(8080)
