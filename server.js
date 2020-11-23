9f61d033549dfc3310bcc6582b4b070a4ce2f4c4
Julia <daubertjulia@yahoo.com>
2020-11-23 20:21:34 +0800

    Server.js updated to acc heroku

--- server.js
+++ server.js
@@ -1,62 +1,41 @@
-
-// *** Dependencies
-// =============================================================
+// Dependencies
 var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
 
-const { Sequelize, DataTypes } = require('sequelize');
+// Create an instance of the express app.
+var app = express();
 
-
-const dbUrl = "mysql://o1yzhgsbu322d80o:wifwril419nvylax@aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zvmhf1j0e2il9o1o"
-
-const sequelize = new Sequelize(dbUrl)
-const User = sequelize.define(
-    'User',
-    {
-        firstName: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        lastName: {
-            type: DataTypes.STRING
-        }
-    }
-);
-
-async function connectAndInit() {
-    await sequelize.authenticate()
-    console.log(`Connected succesfully to ${dbUrl}`)
-    await User.sync({force: false})
-}
-
-async function createUser() {
-    const user = User.build({ firstName: 'Mark', lastName: 'Twain' });
-    await user.save()
-    console.log(`User ${user} saved`)
-}
-
-async function getUsers() {
-    return await User.findAll()
-}
-
-async function main() {
-    await connectAndInit()
-    await createUser()
-    const users = await getUsers();
-    users.forEach( user => 
-        console.log(`${user.firstName} - ${user.lastName}`)
-    )
-}
-
-main()
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
 
 
-const app = express()
- 
-app.get('/', async (req, res) =>{
-  let users = await getUsers()
-  res.send(users)
-})
- 
-//Heroku requires 8080
-app.listen(process.env.PORT)
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");;
+
+app.use(routes);
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
