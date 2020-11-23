439850c53cd484622e0a5f36965471fb9c31f34a
Julia <daubertjulia@yahoo.com>
2020-11-23 17:13:05 +0800

    db credentials

--- server.js
+++ server.js
@@ -1,13 +1,62 @@
+
 // *** Dependencies
 // =============================================================
 var express = require("express");
 
+const { Sequelize, DataTypes } = require('sequelize');
+
+
+const dbUrl = "mysql://o1yzhgsbu322d80o:wifwril419nvylax@aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zvmhf1j0e2il9o1o"
+
+const sequelize = new Sequelize(dbUrl)
+const User = sequelize.define(
+    'User',
+    {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING
+        }
+    }
+);
+
+async function connectAndInit() {
+    await sequelize.authenticate()
+    console.log(`Connected succesfully to ${dbUrl}`)
+    await User.sync({force: false})
+}
+
+async function createUser() {
+    const user = User.build({ firstName: 'Mark', lastName: 'Twain' });
+    await user.save()
+    console.log(`User ${user} saved`)
+}
+
+async function getUsers() {
+    return await User.findAll()
+}
+
+async function main() {
+    await connectAndInit()
+    await createUser()
+    const users = await getUsers();
+    users.forEach( user => 
+        console.log(`${user.firstName} - ${user.lastName}`)
+    )
+}
+
+main()
+
 
 const app = express()
  
-app.get('/', function (req, res) {
-  res.send('This is my Burger App')
+app.get('/', async (req, res) =>{
+  let users = await getUsers()
+  res.send(users)
 })
  
 //Heroku requires 8080
 app.listen(process.env.PORT)
+
